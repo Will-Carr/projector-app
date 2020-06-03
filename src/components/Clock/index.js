740c7b9a43c284c05bea061675714170a628c913
Will-Carr <williamcarr779@gmail.com>
2020-06-03 00:36:46 -0400

    Clock component

---
+++ src/components/Clock/index.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import "./Clock.css";
+
+const Clock = () => {
+  
+  const [currentTime, setCurrentTime] = useState("00:00:00");
+
+  useEffect(() => {
+    // Function to get the current time
+    const getCurrentTime = () => {
+      const date = new Date();
+      const hours = date.getHours() % 12 == 0 ? 12 : date.getHours();
+      const minutes = date.getMinutes().toString().padStart(2, "0");
+      const seconds = date.getSeconds().toString().padStart(2, "0");
+      return `${hours}:${minutes}:${seconds}`;
+    }
+
+    // Initialize with the time
+    setCurrentTime(getCurrentTime());
+
+    // Every second, update
+    const interval = setInterval(() => {
+      setCurrentTime(getCurrentTime());
+    }, 1000);
+
+    // Destroy the interval on cleanup
+    return () => {
+      clearInterval(interval);
+    };
+  }, [setCurrentTime]);
+
+  return (
+    <div id="clock">
+      <span>{currentTime}</span>
+    </div>
+  );
+}
+
+export default Clock;
--- src/components/MainBar/index.js
+++ src/components/MainBar/index.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Clock from "../Clock";
 import "./MainBar.css";
 
 const MainBar = () => {
+
+  const [currentComponentName, setCurrentComponentName] = useState("clock");
+  const [currentComponent, setCurrentComponent] = useState();
+
+  // Update which component we're displaying
+  useEffect(() => {
+    switch(currentComponentName) {
+      case "clock":
+      default:
+        setCurrentComponent(<Clock />);
+        break;
+    }
+  }, [currentComponentName, setCurrentComponent]);
+
   return (
     <div id="main-bar">
       <div id="content-container">
-        
+        {currentComponent}
       </div>
     </div>
   );

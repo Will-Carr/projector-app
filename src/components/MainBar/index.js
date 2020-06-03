e073c6ac488c07958a66b2a5cb170b3d58b506b4
Will-Carr <williamcarr779@gmail.com>
2020-06-03 00:00:18 -0400

    Basic structure

---
+++ src/components/MainBar/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./MainBar.css";
+
+const MainBar = () => {
+  return (
+    <div id="main-bar">
+      <div id="content-container">
+        
+      </div>
+    </div>
+  );
+}
+
+export default MainBar;
